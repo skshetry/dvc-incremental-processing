a244a791dc11137e6600770a481219d855a8d783
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-02-10 16:23:36 +0545

    init

---
+++ collect.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import shutil
+import yaml
+
+with open("params.yaml") as f:
+    params = yaml.safe_load(f)
+dataset = params["dataset_list"]
+
+shutil.copytree("out-temp", "out")
+for path in Path("out").iterdir():
+    if path.name not in dataset:
+        print("Removing", path)
+        path.unlink()
